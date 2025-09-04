171beae8be37061b2b214e09104756d5e8e869b5
joSs-cmd <josa0718@storsjogymnasiet.se>
2025-09-04 11:11:26 +0200

    Update biljett.py

--- intro/biljett.py
+++ intro/biljett.py
@@ -3,13 +3,11 @@ print("hur gammal är du")
 age = input()
 age = int(age)
 
-if age>= 1 and age <6:
+if age <6:
     print("det kommer kosta 11kr")
 
-if age >19:
+if  age >= 18 and age <64:
     print("det kommer kosta 33kr")
 
 if age >65:
     print("det kommer kosta 21 kr")
-
-
